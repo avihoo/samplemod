42f5969d569c3f5207c20e1804ea149f15c9b884
avihoo <avihoo@bitbucket.org>
2016-12-21 11:38:40 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -11,11 +11,12 @@ with open('LICENSE') as f:
 
 setup(
     name='sample_python_package',
-    version='0.0.1',
+    version='0.0.3',
     description='Sample package for Python',
     long_description=readme,
     author='John Doe',
     author_email='john@doe.com',
+    url='https://github.com/avihoo/samplemod',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
